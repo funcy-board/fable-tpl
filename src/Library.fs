6efb9a922d4da518e784245f9994b8e82e655c4b
maestrow <andrew.shmelev@gmail.com>
2018-06-02 18:19:11 +0300

    Update webpack.config to correctly manage Hot Module Replacement.

--- src/Library.fs
+++ src/Library.fs
@@ -2,4 +2,9 @@ namespace FableApp
 
 module Say =
     let hello name =
-        printfn "Hello %s" name
+        printfn "Hello! %s" name
+
+    [<EntryPoint>]
+    let main args = 
+        hello "Fable"
+        0
